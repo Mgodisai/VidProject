4e2cbd9df145f3786d02b963586dbb9d782e8a7f
Varga Tamás <mgodisai@gmail.com>
2022-03-30 19:41:50 +0200

    Add project files.

---
+++ VidProject/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace VidProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxMain = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxMain
+            // 
+            this.tbxMain.Location = new System.Drawing.Point(98, 64);
+            this.tbxMain.Multiline = true;
+            this.tbxMain.Name = "tbxMain";
+            this.tbxMain.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxMain.Size = new System.Drawing.Size(256, 137);
+            this.tbxMain.TabIndex = 0;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(98, 236);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(279, 236);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(189, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.tbxMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxMain;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ VidProject/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using GetID;
+
+namespace VidProject
+{
+    public partial class Form1 : Form
+    {
+        readonly GetID.GetID egyes;
+        static string strRegexID = "^[A-Z][0-9]{4}$";
+
+        public Form1()
+        {
+            InitializeComponent();
+            egyes = new GetID.GetID();
+            egyes.ValueChanged += getID_ValueChange;
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            egyes.Go();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            egyes.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tbxMain.Text += egyes.Value+Environment.NewLine;
+        }
+
+        private void getID_ValueChange(object sender, EventArgs e)
+        {
+            string id = egyes.Value;
+            id += idRegexTester(id) ? " (MEGFELELŐ)" : " (NEM MEGFELELŐ)";
+            tbxMain.Invoke((Action)delegate
+            {
+                tbxMain.Text += id + Environment.NewLine;
+            });
+        }
+
+        private bool idRegexTester(string id)
+        {
+            return new Regex(strRegexID).Match(id).Success;
+        }
+    }
+}
